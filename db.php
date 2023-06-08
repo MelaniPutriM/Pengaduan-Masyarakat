ed068813c9e24033de0427dd4d1af6505674cd2f
MelaniPutriM <melanimaharani166@gmail.com>
2023-06-08 16:28:00 +0700

    new

--- db.php
+++ db.php
@@ -2,9 +2,9 @@
 class DB
 {
     private $db_host = "localhost";
-    private $db_user = "root";
-    private $db_pass = "";
-    private $db_name = "cba";
+    private $db_user = "id20881644_melani";
+    private $db_pass = "Melani16!";
+    private $db_name = "id20881644_cbaweb";
 
     public $con = false;
     private $hasil = array();
